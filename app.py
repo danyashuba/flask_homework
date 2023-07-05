8704a1fc6ca767889cc5f0b45a2a0901c3b1883d
danyashuba <shubadanya01@gmail.com>
2023-07-06 00:27:30 +0300

    HW34 done

--- app.py
+++ app.py
@@ -20,35 +20,9 @@ dictConfig({
 
 
 app = Flask(__name__)
+app.secret_key = 'super secret key'
 
-
-@app.route('/')
-def hello_world():  # put application's code here
-    app.logger.info("The site was in process")
-    return (
-        '<h1>Hello World!</h1>'
-    )
-
-
-@app.route('/about')
-def author_info():
-    app.logger.info("The information abot the creator was called")
-    return (
-        '<h1>Danyil Shuba:</h1>'
-        '<p>A student in Kyiv`s national university</p>'
-    )
-
-
-@app.route('/json_info')
-def author_data():
-    app.logger.info("The json format was checked")
-    return {
-        'name': 'Danyil',
-        'surname': 'Shuba',
-        'age': 20,
-        'natinality': 'Ukraine'
-    }
-
+from views import *
 
 if __name__ == '__main__':
     app.run(debug=True)
